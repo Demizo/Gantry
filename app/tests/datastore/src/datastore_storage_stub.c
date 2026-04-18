00c547d9bfe8298094ba57fb5c324d1469f6fcbb
Demizo <demizodemazo@gmail.com>
2026-04-18 10:41:51 -0500

    feat: add datastore unit tests

---
+++ app/tests/datastore/src/datastore_storage_stub.c
@@ -0,0 +1,9 @@
+#include "datastore_storage.h"
+
+int datastore_storage_load(void) { return 0; }
+
+int datastore_storage_save_item(const struct datastore_item_const_metadata* item)
+{
+    (void)item;
+    return 0;
+}
