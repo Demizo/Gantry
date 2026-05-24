bb29e2bb960b3c02e3b45e2d091f898611798e0f
Demizo <demizodemazo@gmail.com>
2026-05-23 23:28:35 -0500

    fix: docs environment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,25 +1,27 @@
-project = 'Gantry'
-copyright = '2026, Demizo'
-author = 'Demizo'
-release = '1.0.0'
+project = "Gantry"
+copyright = "2026, Demizo"
+author = "Demizo"
+release = "1.0.0"
 
 extensions = [
-    'breathe',
-    'sphinxcontrib.mermaid',
-    'sphinx_rtd_theme',
+    "breathe",
+    "sphinxcontrib.mermaid",
+    "sphinx_rtd_theme",
 ]
 
-breathe_projects = {
-    "Firmware": "../doxygen/xml" 
-}
+breathe_projects = {"Firmware": "../doxygen/xml"}
 breathe_default_project = "Firmware"
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 html_show_sphinx = False
+html_logo = "_static/logo_title.png"
+html_theme_options = {
+    "logo_only": True,
+}
 
 html_context = {
     "show_copyright": True,
